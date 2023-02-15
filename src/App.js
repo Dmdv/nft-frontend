b64716ec686b75237293fa7f860e6cd2772e6f27
DmitryD <dima.dyachkov@gmail.com>
2023-02-16 01:45:18 +0800

    Added getNFT function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import {NFTCard} from "./components/NFTCard";
 import {NFTModal} from "./components/NFTModal";
 import styled from "styled-components";
 import {useState} from 'react';
+import {ethers} from "ethers";
+import address from "address";
+
+const axios = require('axios');
 
 function App() {
     const [showModal, setShowModal] = useState(false);
     const [selectedNft, setSelectedNft] = useState();
+    const [nfts, setNfts] = useState(initialNfts);
 
-    const nfts = [
+    const initialNfts = [
         {
             name: "Faraway1", symbol: "FTK", copies: 1, image: "https://via.placeholder.com/600x400.jpg",
             attributes: [
@@ -38,43 +43,89 @@ function App() {
         setShowModal(!showModal);
     }
 
-    return (
-        <div className="App">
-            <Container>
-                <Title>Faraway NFT collection</Title>
-                <SubTitle>The rarest and best</SubTitle>
-                <Grid>
-                    {
-                        nfts.map((nft, index) =>
-                            <NFTCard key={index} nft={nft} toggleModal={() => toggleModal(index)}/>)
-                    }
-                </Grid>
-            </Container>
-            {
-                showModal &&
-                <NFTModal nft={selectedNft} toggleModal={() => toggleModal()}/>
-            }
-        </div>
-    );
-}
+    async function getNFTs(address) {
+        const INFURA_API_KEY = process.env.REACT_APP_INFURA_API_KEY;
+        const rpc = `https://goerli.infura.io/v3/${INFURA_API_KEY}`;
 
-const Title = styled.h1`
-  margin: 0;
-  text-align: center
-`
-const SubTitle = styled.h4`
-  color: gray;
-  margin-top: 0;
-  text-align: center
-`
-const Container = styled.div`
-  width: 70%;
-  max-width: 1200px;
-  margin: 100px auto auto;
-`
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  row-gap: 40px;
-`
-export default App;
+
+        const ethersProvider = new ethers.providers.JsonRpcProvider(rpc);
+
+        const abi = [
+            "function name() public view returns (string memory)",
+            "function symbol() public view returns (string memory)",
+            "function tokenURI(uint256 tokenId) view returns (string memory)",
+            "function totalSupply() external view returns (uint256);",
+        ];
+
+        const nftCollection = new ethers.Contract(address, abi, ethersProvider);
+        const numberOfNft = (await nftCollection.totalSupply()).to_number();
+        const symbol = await nftCollection.symbol();
+        const name = await nftCollection.name();
+        const accounts = Array(numberOfNft).fill(address);
+        const ids = Array.from({length: numberOfNft}, (_, i) => i + 1);
+
+        let tempArray = [];
+        let baseUrl = "";
+
+        for (let i = 1; i <= numberOfNft; i++) {
+            if (i == 1) {
+                const uri = await nftCollection.tokenURI(i);
+                const baseUrl = uri.replace("/\d+.json/", "");
+                const response = await axios.get(uri);
+                const nft = response.data;
+                nft.symbol = symbol;
+                nft.name = name;
+                nft.copies = 1; // Our version allows only unique NFTs
+                tempArray.push(nft);
+            } else {
+                const response = await axios.get(baseUrl + `${i}.json`);
+                const nft = response.data;
+                nft.symbol = symbol;
+                nft.name = name;
+                nft.copies = 1; // Our version allows only unique NFTs
+                tempArray.push(nft);
+            }
+        }
+
+        setNfts(tempArray);
+
+        return (
+            <div className="App">
+                <Container>
+                    <Title>Faraway NFT collection</Title>
+                    <SubTitle>The rarest and best</SubTitle>
+                    <Grid>
+                        {
+                            nfts.map((nft, index) =>
+                                <NFTCard key={index} nft={nft} toggleModal={() => toggleModal(index)}/>)
+                        }
+                    </Grid>
+                </Container>
+                {
+                    showModal &&
+                    <NFTModal nft={selectedNft} toggleModal={() => toggleModal()}/>
+                }
+            </div>
+        );
+    }
+
+    const Title = styled.h1`
+      margin: 0;
+      text-align: center
+    `
+    const SubTitle = styled.h4`
+      color: gray;
+      margin-top: 0;
+      text-align: center
+    `
+    const Container = styled.div`
+      width: 70%;
+      max-width: 1200px;
+      margin: 100px auto auto;
+    `
+    const Grid = styled.div`
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      row-gap: 40px;
+    `
+    export default App;
